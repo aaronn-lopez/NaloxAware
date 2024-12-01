3905d1afc202427267d46428dcf25f6a1a44a96e
bowerbankk <ericbowerban@gmail.com>
2024-11-30 17:06:32 -0800

    slide in animation only happens when content is scrolled to

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//mobile navbar toggle button
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
@@ -6,6 +7,7 @@ menu.addEventListener('click', function() {
     menuLinks.classList.toggle('active');
 });
 
+//bio panels for team members
 function openBio(name, role, content) {
     const panel = document.getElementById('bio-panel');
     document.getElementById('bio-name').textContent = name;
@@ -19,3 +21,20 @@ function closeBio() {
     const panel = document.getElementById('bio-panel');
     panel.classList.remove('open');
 }
+
+//slide in animations for content, triggered by scrolling to it
+var observer = new IntersectionObserver(function(entries) {
+    entries.forEach(
+        (element)=>{
+            if(element.isIntersecting){
+                element.target.classList.add('should__animate__slide-in');
+                console.log(element.target.className);
+            }
+        }
+    );
+}, { threshold: [0.5] });
+
+var contents=document.getElementsByClassName('main__content');
+Array.from(contents).forEach(element => {
+    observer.observe(element);
+});
